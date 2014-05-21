5a49d1def54624ed404989bdceb9da9d9ae29d54
yami@crimsondream.jp <yami@crimsondream.jp>
2014-05-21 15:01:24 +0900

    long description追加

--- README.rst
+++ README.rst
@@ -20,5 +20,5 @@ Installation
 
 The last stable release is available on PyPI and can be installed with ``pip``::
 
-    $ pip install spreadsheetconverter
+    $ pip install SpreadsheetConverter
 
--- setup.py
+++ setup.py
@@ -4,6 +4,13 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 from setuptools import setup, find_packages
 
+
+try:
+    with open('README.rst') as f:
+        readme = f.read()
+except IOError:
+    readme = ''
+
 setup(
     name="SpreadsheetConverter",
     version='0.0.1',
@@ -13,6 +20,7 @@ setup(
     maintainer='yamionp',
     maintainer_email='yami@crimsondream.jp',
     description='Spreadsheet Converter',
+    long_description=readme,
     license="MIT",
     packages=find_packages(),
     install_requires=open('requirements.txt').read().splitlines(),
