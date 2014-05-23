e1587e7b1cfe738f3441499ed893ca359c3eb35c
yami@crimsondream.jp <yami@crimsondream.jp>
2014-05-23 16:56:54 +0900

    verup

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except IOError:
 
 setup(
     name="SpreadsheetConverter",
-    version='0.0.3',
+    version='0.0.4',
     url='https://github.com/yamionp/spreadsheetconverter/',
     author='yamionp',
     author_email='yami@crimsondream.jp',
--- spreadsheetconverter/__init__.py
+++ spreadsheetconverter/__init__.py
@@ -5,5 +5,5 @@ from .config import Config, YamlConfig
 from .converter import Converter
 
 
-VERSION = (0, 0, 3, None)
+VERSION = (0, 0, 4, None)
 __version__ = '.'.join(map(str, VERSION))
